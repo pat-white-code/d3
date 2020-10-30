4fda6a0cea54adf2dbac246ae5c9dd9960c8467a
pat-white-code <pat.k.white@gmail.com>
2020-10-30 15:15:03 -0500

    adds labels

--- drawing-with-data.js
+++ drawing-with-data.js
@@ -18,3 +18,18 @@ svg.selectAll("rect")
   .attr('width', w / data.length - padding)
   .attr('height', (d) => d * 2)
   .attr('fill', d=> `rgb(${d * .5}, ${d * .75}, ${d * .25})`)
+
+svg.selectAll('text')
+  .data(data)
+  .enter()
+  .append('text')
+  .text(d => d)
+  .attr('text-anchor', 'middle')
+  .attr('x', (d, i)=> i * (w / data.length) + (w / data.length - padding) / 2)
+  .attr('fill', 'white')
+  .attr('y', (d) => h - d * 2 + 18)
+  .attr('font-family', 'sans-serif')
+  .attr({
+    x: (d, i) => i * (w / data.length),
+    y: d => h - d * 2
+  })
