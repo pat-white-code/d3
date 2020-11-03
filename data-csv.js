d57cd14768f7026e79fc804b497a42bcf4438c5d
pat-white-code <pat.k.white@gmail.com>
2020-11-02 21:13:48 -0600

    customizes axes

--- data-csv.js
+++ data-csv.js
@@ -9,7 +9,7 @@ console.log(h, w)
 const render = data => {
   const xVal = d => d.population
   const yVal = d => d.country
-  const margin = {top: 50, right: 50, bottom: 50, left: 80}
+  const margin = {top: 50, right: 50, bottom: 50, left: 100}
   const innerWidth = w - margin.left - margin.right;
   const innerHeight = h - margin.top - margin.bottom;
 
@@ -33,8 +33,12 @@ const render = data => {
     .tickFormat(xAxisTickFormat)
 
   g.append('g').call(d3.axisLeft(yScale))
+    .selectAll('.domain, .tick line').remove()
+
   g.append('g').call(xAxis)
   .attr('transform', `translate(0, ${innerHeight})`)
+  .attr('class', 'x-axis-lab')
+  .select('.domain').remove();
 
   g.selectAll('rect').data(data)
     .enter().append('rect')
@@ -42,6 +46,10 @@ const render = data => {
       .attr('height', yScale.bandwidth())
       // .attr('height', 300)
       .attr('width', d => xScale(xVal(d)))
+  
+  g.append('text')
+    .text('10 Most Populous Countries')
+    .attr('class', 'chart-title')
 }
 
 d3.csv('data.csv').then(data => {
